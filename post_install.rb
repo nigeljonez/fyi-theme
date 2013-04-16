ad0bd398cfb33ff12bc52810b6580b0022f7b760
Rowan Crawford <wombleton@gmail.com>
2013-04-16 22:52:58 +1200

    Update translations if different

--- post_install.rb
+++ post_install.rb
@@ -1,1 +1,7 @@
 # This file is executed in the Rails evironment by the `rails-post-install` script
+
+PublicBody.each{|body|
+    body.translations.each{|translation|
+        translation.update_attribute(:locale, "en") if translation.locale == "en_NZ"
+    }
+}
