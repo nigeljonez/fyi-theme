10dcdfa0b1d10a3cad6f8006f8b01f6938be15ca
Rowan Crawford <wombleton@gmail.com>
2014-02-10 10:17:18 +1300

    Remove old update_attribute in post-install

--- post_install.rb
+++ post_install.rb
@@ -1,7 +1,1 @@
 # This file is executed in the Rails evironment by the `rails-post-install` script
-
-PublicBody.each{|body|
-    body.translations.each{|translation|
-        translation.update_attribute(:locale, "en") if translation.locale == "en_NZ"
-    }
-}
