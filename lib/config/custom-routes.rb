ca5a79d49c8313455076a47e2400d613632708f3
Nigel Jones <njones@nigelj.nz>
2019-07-07 16:15:04 +1200

    Override /help/requesting

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # Additional help page example
   # get '/help/help_out' => 'help#help_out'
   match "/help" => redirect("https://meta.fyi.org.nz/help"), :via => :get
+  match "/help/about" => redirect("https://meta.fyi.org.nz/help/about"), :as => :help_about_new, :via => :get
   match "/help/general" => redirect("https://meta.fyi.org.nz/help/about"), :as => :help_gen, :via => :get
+  match "/help/requesting" => redirect("https://meta.fyi.org.nz/help/making-requests"), :as => :help_requesting_new, :via => :get
 end
